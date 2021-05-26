17390b1f1ccd62f3b0f733a4b6306db3caa61185
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-05-26 22:06:02 +0200

    second API call with fetch working

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,12 +1,31 @@
-export const BASE_API_URL = "https://hacker-news.firebaseio.com/v0";
+import { BASE_URL } from "./constants";
 
+// Get info about every story
+const getStory = async (id) => {
+  try {
+    // Getting info about one story
+    const response = await fetch(`${BASE_URL}/item/${id}.json`);
+    // Resolving the promise
+    const story = await response.json();
+    console.log(story);
+    return story
+  } catch (error) {
+    console.log("Api error(single story)");
+  }
+};
+
+// Getting the ID's of top stories
 export const getStories = async () => {
   try {
-    const response = await fetch(`${BASE_API_URL}/topstories.json`);
-    console.log(response);
+    // Getting all the ID/s
+    const response = await fetch(`${BASE_URL}/topstories.json`);
+    // Getting the response in json
     const data = await response.json();
-    console.table(data);
-    return data;
+    // Getting info about every story
+    const stories = await Promise.all(
+      data.slice(0, 30).map((id) => getStory(id))
+    );
+    return stories;
   } catch (error) {
     console.log("Api error");
   }
