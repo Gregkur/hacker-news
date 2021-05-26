caee07cd1f6a8c4c8f9ae5daf7e9e0f5caaa6570
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-05-26 21:11:45 +0200

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
-import Story from "./components/Story";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import HomePage from "./components/HomePage";
+import Comments from "./components/Comments";
 
 function App() {
+  const findStory = (id) => {
+    console.log("TODO", id);
+  };
+
   return (
-    <div className="container">
-      <h1>Hacker News</h1>
-      <div className="story-container">
-        <Story />
-        <Story />
-        <Story />
-        <Story />
+    <BrowserRouter>
+      <div className="container">
+        <h1>Hacker News</h1>
+        <Switch>
+          <Route path="/" component={HomePage} exact={true} />
+          <Route
+            path="/stories/:id"
+            render={(routeProps) => (
+              <Comments
+                story={findStory(routeProps.match.params.id)}
+                {...routeProps}
+              />
+            )}
+          />
+        </Switch>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Story from "./Story";
+
+const HomePage = () => {
+  return (
+    <div className="story-container">
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+    </div>
+  );
+};
+export default HomePage;
