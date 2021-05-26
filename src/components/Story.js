6f89886f68da9c2f18d4fd6d47b82cd889813f3b
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-05-26 23:13:59 +0200

    finished story component without styling

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import unixToDate from "../helpers/unix";
 
 import "../styles/StoryStyles.css";
 
-const Story = ({ data: { by, title, descendants, time } }) => {
+const Story = ({ data: { by, title, descendants, time, url, id } }) => {
   return (
     <div className="story">
       <div className="story-title">
-        <p>{title}</p>
+        <a href={url} target="_blank" rel="noreferrer">
+          {title}
+        </a>
       </div>
       <div className="story-info">
         <p>
-          by: {by} | {unixToDate(time)} | {descendants} comments
+          by: {by} | {unixToDate(time)} |{" "}
+          <Link to={`/stories/${id}`}>{descendants} comments</Link>
         </p>
       </div>
     </div>
