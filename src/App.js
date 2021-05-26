c195ada09e5c2bb14c5ccc26e56059b727cfce18
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-05-26 20:14:49 +0200

    basic html structure to test the fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Story from "./components/Story";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hacker News</h1>
+      <div className="story-container">
+        <Story />
+        <Story />
+        <Story />
+        <Story />
+      </div>
     </div>
   );
 }
---
+++ src/components/Story.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/StoryStyles.css";
+
+const Story = () => {
+  return (
+    <div className="story">
+      <div className="story-title">
+        <p>Neovim 0.5 is overpowering</p>
+      </div>
+      <div className="story-info">
+        <p>by imbnwa | 5/26/2021, 4:56 PM | 31 comments</p>
+      </div>
+    </div>
+  );
+};
+export default Story;
